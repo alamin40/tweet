5a9dbd4ab0330597be3dcc794c389de84d7308e5
alamin40 <hossain.78295@gmail.com>
2024-08-18 02:12:44 +0200

    adding create and edit code for tweet

--- Story/tweet/views.py
+++ Story/tweet/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from .models import Tweet
 from .forms import TweetForm
-from django.shortcuts import get_object_or_404
+from django.shortcuts import get_object_or_404, redirect
 
 # Create your views here.
 
@@ -12,3 +12,29 @@ def index(request):
 def tweet_list(request):
     tweets = Tweet.objects.all().order_by('-created_at')
     return render(request, 'tweet_list.html', {'tweets': tweets})
+
+
+def tweet_create(request):
+    if request.method == "POST":
+        form = TweetForm (request.POST, request.FILES)
+        if form.is_valid():
+            tweet = form.save(commit=False)
+            tweet.user = request.user
+            tweet.save()
+            return redirect('tweet_list')
+    else:
+        form = TweetForm()
+    return render (request, 'tweet_form.html', {'form': form})
+
+def tweet_edit(request, tweet_id):
+    tweet = get_object_or_404(Tweet, pk=tweet_id, user = request.user)
+    if request.method == 'POST':
+        form = TweetForm (request. POST, request.FILES, instance=tweet)
+        if form.is_valid():
+            tweet = form.save(commit=False)
+            tweet.user = request.user
+            tweet.save()
+            return redirect('tweet_list')
+    else:
+        form = TweetForm (instance=tweet)
+    return render (request, 'tweet_form.html', {'form': form})
